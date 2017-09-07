52da1a65345bc8d1a3d96059a2172f6592693e4e
markjiang <markjiang@live.com>
2017-09-07 22:05:11 +0800

    use storage under /var/log

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,6 +41,11 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
+$customPath = '/var/log/ec-api';
+if (file_exists($customPath)) {
+    $app->useStoragePath($customPath);
+}
+
 /*
 |--------------------------------------------------------------------------
 | Return The Application
