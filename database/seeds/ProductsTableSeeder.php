11782ef57a9486a619dc81832e03323fea2ab43a
markjiang <markjiang@live.com>
2017-09-14 00:45:15 +0800

    product table migration and seeder

---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Models\Product::class, 50)->make()->each(function($product){
+            $product->save();
+        });
+    }
+}
